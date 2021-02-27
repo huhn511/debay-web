f673b56f9226ea87a72d517702e5f614eaae7160
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:28:12 +0100

    adds pure styles Button ui-component

---
+++ src/components/Button.svelte
@@ -0,0 +1,18 @@
+<style>
+  button {
+		background-color: rgba(52, 116, 235);
+		outline: none;
+		border-radius: 100px;
+		border: 2px solid rgba(0,0,0,.1);
+		padding: 1em 2em;
+		font-size: 1em;
+		color: white;
+		display: block;
+		width: 100%;
+		font-weight: bold;
+	}
+</style>
+
+<button>
+	<slot></slot>
+</button>
