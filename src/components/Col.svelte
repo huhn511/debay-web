36ca3557deedbcc87dfa924e5e63d78383ac9a47
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:27:10 +0100

    adds Col and Row ui component

---
+++ src/components/Col.svelte
@@ -0,0 +1,18 @@
+<style>
+  .col {
+		flex: 1;
+		width: 100%;
+		min-width: 100%;
+	}
+	
+	@media (min-width: 720px) {
+		.col {
+			margin: 0 15px;
+
+    }
+	}
+</style>
+
+<div class="col">
+  <slot></slot>
+</div>
---
+++ src/components/Row.svelte
@@ -0,0 +1,17 @@
+<style>
+  .row {
+		display: flex;
+    flex-wrap: wrap;
+	}
+  @media (min-width: 720px) {
+    .row {
+      justify-content: space-between;
+      margin: 0 -15px;
+      flex-wrap: nowrap;
+    }
+	}
+</style>
+
+<div class="row">
+  <slot></slot>
+</div>
