7512b170d977f026c6dc8b292bdab8ba104fb344
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:27:19 +0100

    adds Image ui component

---
+++ src/components/Image.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let imageUrl
+</script>
+
+<style>
+  .image {
+    background-size: cover;
+    width: 100%;
+    height: 300px;
+    margin-bottom: 30px;
+    border-radius: 10px;
+  }
+</style>
+
+<div 
+  class="image" 
+  style="background-image: url('{imageUrl}')"
+/>
