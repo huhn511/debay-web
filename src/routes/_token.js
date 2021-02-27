08e7c65e06540155344785be36521ece0eb4ad8c
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:26:28 +0100

    adds token mock data

---
+++ src/routes/_token.js
@@ -0,0 +1,17 @@
+const token =	{
+	id: '1231238712398712983',
+	name: 'ERC20 Die Erste',
+	description: 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien.',
+	imageUrl: 'https://images.pexels.com/photos/1145720/pexels-photo-1145720.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+	price: {
+		unit: 'MI',
+		value: 100
+	},
+	owner: {
+		id: '12391293081029381',
+		imageUrl: 'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+		name: 'Johanna Doe'
+	}
+};
+
+export default token;
