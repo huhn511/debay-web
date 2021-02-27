e6cc291a37b69f9b793bee36b42fc83c89eb8ca7
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:27:45 +0100

    adds Price ui component

---
+++ src/components/Price.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let value
+  export let unit
+</script>
+
+<style>
+  .price {
+		display: flex;
+		align-items: flex-end;
+		margin-bottom: 20px;
+	}
+
+	h3 {
+		font-size: 2em;
+		font-weight: bold;
+		margin: 0 5px 0 0;
+	}
+
+	span {
+		margin-bottom: 2px;
+	}
+</style>
+
+<div class="price">
+	<h3>{value}</h3>
+	<span>{unit}</span>
+</div>
