aafb3e75b5a430b32af4d76c985d4ca5f8cdd0ce
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:27:33 +0100

    adds Separator ui component

---
+++ src/components/Separator.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let title
+</script>
+
+<style>
+  span {
+		text-transform: uppercase;
+		font-size: 14px;
+		letter-spacing: 0.1em;
+		font-weight: bold;
+		margin-bottom: 5px;
+		opacity: .5;
+	}
+</style>
+
+<span>
+	{title}
+</span>
