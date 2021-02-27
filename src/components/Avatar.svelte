4d275a5f4c24e3e93161507bf5cee94e6d041df9
Sven Reiser <svreiser@gmail.com>
2021-02-28 00:26:54 +0100

    adds Avatar ui component

---
+++ src/components/Avatar.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let imageUrl
+  export let name
+</script>
+
+<style>
+  
+  .avatar {
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+  }
+
+  .avatar-image {
+    border-radius: 30px;
+    height: 30px;
+    width: 30px;
+    margin-right: 15px;
+    background-size: cover;
+  }
+
+</style>
+
+<div class="avatar">
+  <div 
+    class="avatar-image"
+    style="background-image: url('{imageUrl}')"
+  />
+  <span>{name}</span>
+</div>
