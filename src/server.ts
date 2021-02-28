05474c6c98f3a8657ef4730b1d16f0b902272a11
Sven Reiser <svreiser@gmail.com>
2021-02-28 03:22:10 +0100

    fixes path

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development'
 
 polka() // You can also use Express
 	.use(
-		dev ? "/" : "debay-web",
+		dev ? "/" : "/debay-web/",
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
