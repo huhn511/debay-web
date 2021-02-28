28320894dd864a040e90d6250b9392460534c474
Sven Reiser <svreiser@gmail.com>
2021-02-28 21:52:26 +0100

    removes vercel and gh-pages config

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const dev = NODE_ENV === 'development'
 
 polka() // You can also use Express
 	.use(
-		dev ? "/" : "/debay-web/",
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
