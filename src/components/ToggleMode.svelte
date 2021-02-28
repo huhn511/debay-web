b98d68cabf43f75fa8698ead4911ff9480980e70
Sven Reiser <svreiser@gmail.com>
2021-02-28 01:42:06 +0100

    adds global styles and ToggleMode (light and dark)

---
+++ src/components/ToggleMode.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Moon from './icons/Moon.svelte'
+
+  function toggle() {
+    window.document.body.classList.toggle('dark-mode')
+  }
+</script>
+
+<style>
+  button {
+		background-color: rgba(52, 116, 235);
+		outline: none;
+		border-radius: 100px;
+		border: 2px solid rgba(0,0,0,.1);
+		width: 50px;
+		height: 50px;
+		font-size: 1em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		cursor: pointer;
+	}
+</style>
+
+<button on:click={toggle}>
+	<Moon />
+</button>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,19 +1,57 @@
-<script lang="ts">
-
-	export let segment: string;
+<script>
+	import Nav from '../components/Nav.svelte'
 </script>
 
 <style>
-	main {
+	:global(:root){
+		--color-white: white;
+		--color-light: #efefef;
+		--color-dark: #2e2e2e;
+		--color-black: #181818;
+		--color-primary: rgba(52, 116, 235);
+		--color-page: var(--color-light);
+		--color-button: var(--color-primary);
+		--color-text: var(--color-black);
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+	:global(body) {
+		background-color: var(--color-page);
+	}
+	
+	:global(main) {
+		top: 90px;
 		position: relative;
 		max-width: 56em;
-		background-color: white;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
+
+	:global(h1) {
+		font-weight: bold;
+		font-size: 2.5em;
+	}
+
+	:global(p) {
+		font-size: 1em;
+	}
+
+	:global(body) {
+		background-color: var(--color-white);
+		color: var(--color-black);
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: var(--color-black);
+		color: var(--color-white);
+	}
 </style>
 
 <main>
-	<slot></slot>
+	<Nav />
+	
+	<slot/>
 </main>
